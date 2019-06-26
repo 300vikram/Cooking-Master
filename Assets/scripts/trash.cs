21b601f9ae1014bd2c3c3166838f3e57f468e1fc
Vikram Sharma <300vikram@gmail.com>
2019-06-26 05:39:15 +0530

    Restart Button Bug

--- Assets/scripts/trash.cs
+++ Assets/scripts/trash.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class trash : MonoBehaviour
 {
     public GameObject gameoverpanel;
     public Text gameOverText;
 
+    //public GameObject restartbutton;
+    public Button restartbutton;
+    public Text restarttext;
+   
+
     // Start is called before the first frame update
     void Start()
     {
-
+        restartbutton.gameObject.SetActive(false);
         gameoverpanel.SetActive(false);
-
+        
     }
 
     // Update is called once per frame
@@ -23,12 +29,14 @@ public class trash : MonoBehaviour
         {
             gameoverpanel.SetActive(true);
             gameOverText.text = "Player 1 Wins";
+            restartbutton.gameObject.SetActive(true);
         }
 
         if ((timep1.timeLeft <= 0) && (timep2.timeLeft <= 0) && (gamePlay.score1 < gamePlay.score2))
         {
             gameoverpanel.SetActive(true);
             gameOverText.text = "Player 2 Wins";
+            restartbutton.gameObject.SetActive(true);
         }
 
     }
@@ -42,5 +50,12 @@ public class trash : MonoBehaviour
         }
 
         Debug.Log(gamePlay.deletefood);
+    }
+
+    public void restart()
+    {
+        gamePlay.score1 = 0;
+        gamePlay.score2 = 0;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
